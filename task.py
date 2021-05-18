95e5266b518ef4091c6cf34864ef793478b83d8b
Michael Kistler <michaelkis213@yahoo.com>
2021-05-18 10:52:09 -0400

    Create skeleton of functions needed

---
+++ task.py
@@ -0,0 +1,40 @@
+def conv_num(num_str):
+    """
+    Takes a string as input and converts the string into a number and
+    returns the number. Returns False if the string is not a valid number.
+    """
+    pass
+
+
+def valid_number(num_str):
+    """
+    Takes a string as input and returns 'integer' if the string is a valid
+    integer, 'decimal' if the string is a valid decimal number, or
+    'hexadecimal' if the string is a valid hexadecimal number.
+    Returns False otherwise.
+    """
+    pass
+
+
+def valid_integer(num_str):
+    """
+    Takes a string as input and returns True if the string represents a valid
+    integer. Returns False otherwise.
+    """
+    pass
+
+
+def valid_decimal(num_str):
+    """
+    Takes a string as input and returns True if the string represents a valid
+    decimal number. Returns False otherwise.
+    """
+    pass
+
+
+def valid_hexadecimal(num_str):
+    """
+    Takes a string as input and returns True if the string represents a valid
+    hexadecimal number. Returns False otherwise.
+    """
+    pass
